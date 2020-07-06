3ce02ac4d1766cc02f300e45b8d03737d313e4f3
InGyum <kig6416@naver.com>
2020-07-06 09:50:04 +0900

    2020/07/06

---
+++ StudyCSharp/EnumApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumApp
+{
+    class Program
+    {
+        enum DialogResult
+        {
+            YES,
+            NO,
+            CANCEL,
+            CONFIRM,
+            OK
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(DialogResult.OK);
+            //Console.WriteLine((int)DialogResult.OK);
+            DialogResult result = DialogResult.CONFIRM;
+            if(result==DialogResult.CONFIRM)
+            {
+                Console.WriteLine("CONFIRM 실행");
+            }
+        }
+    }
+}
